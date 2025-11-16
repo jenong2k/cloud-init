4a51d2df7c0e1c604bb3faef9df1adff8afa857d
Rudi <jenong@gmail.com>
2025-11-17 06:09:51 +0700

    update

--- roles/ubuntu/templates/netplan.sh
+++ roles/ubuntu/templates/netplan.sh
@@ -4,21 +4,20 @@
 {% for interface in ansible_interfaces | sort %}
 
 {% if ansible_facts[interface]['macaddress'] is defined %}
+
 # {{ ansible_facts[interface]['ipv4'] }}
-# {{ ansible_facts[interface]['ipv6'] }}
 # netplan set ethernets.{{ interface }}.match.macaddress={{ ansible_facts[interface]['macaddress'] }}
 # netplan set ethernets.{{ interface }}.set-name={{ interface }}
-# netplan set ethernets.{{ interface }}.addresses=["{{ ansible_facts[interface]['ipv4']['address'] }}/{{ ansible_facts[interface]['ipv4']['prefix'] }}"]
-# netplan set ethernets.{{ interface }}.addresses=["{{ ansible_facts[interface]['ipv6'][0]['address'] }}/{{ ansible_facts[interface]['ipv6'][0]['prefix'] }}"]
-# netplan set ethernets.{{ interface }}.routes='[{to: "default", via: "{{ ansible_default_ipv4.gateway }}"}]'
-# netplan set ethernets.{{ interface }}.routes='[{to: "{{ ansible_facts[interface]['ipv4']['network'] }}/{{ ansible_facts[interface]['ipv4']['prefix'] }}", via: "{{ ansible_default_ipv4.gateway }}"}]'
+{% endif %}
+
+# netplan set ethernets.{{ interface }}.addresses=[""]
+# netplan set ethernets.{{ interface }}.routes='[{to: "default", via: ""}]'
 # netplan set ethernets.{{ interface }}.accept-ra=false
 # netplan set ethernets.{{ interface }}.dhcp4=false
 # netplan set ethernets.{{ interface }}.dhcp6=false
 # netplan set ethernets.{{ interface }}.dhcp4-overrides.use-routes=false
 # netplan set ethernets.{{ interface }}.dhcp6-overrides.use-routes=false
-# netplan set ethernets.{{ interface }}.nameservers.addresses=["x"]
-{% endif %}
+# netplan set ethernets.{{ interface }}.nameservers.addresses=[""]
 
 {% endfor %}
 
